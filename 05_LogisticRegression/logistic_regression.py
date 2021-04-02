3c9fa29446707b984f2c33accf76695be6d7cbf6
Geonwoo Kim <kw.kim@xenoimpact.com>
2021-04-02 17:34:24 +0900

    [Feature] - Successfully display the `Linear-Regression` Model

--- 05_LogisticRegression/logistic_regression.py
+++ 05_LogisticRegression/logistic_regression.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 data = [
@@ -9,4 +11,30 @@ y_data = [i[1] for i in data]
 plt.scatter(x_data, y_data)
 plt.xlim(0, 15)
 plt.ylim(-.1, 1.1)
+
+a = b = 0
+
+lr = 0.05
+
+
+def sigmoid(x):
+    return 1 / (1 + np.e ** (-x))
+
+for i in range(2001):
+    for x_data, y_data in data:
+        a_diff = x_data * (sigmoid(a * x_data + b) - y_data)
+        b_diff = sigmoid(a * x_data + b) - y_data
+        a = a - lr * a_diff
+        b = b - lr * b_diff
+
+        if i % 1000 == 0:
+            print("Epoch = %.f, Gradient = %.04f, Intercept = %.04f" % (i, a, b))
+    
+    plt.scatter(x_data, y_data)
+    plt.xlim(0, 15)
+    plt.ylim(-.1, 1.1)
+    
+    x_range = (np.arange(0, 15, 0.1))
+    plt.plot(np.arange(0, 15, 0.1), np.array([sigmoid(a * x + b)
+for x in x_range]))
 plt.show()
--- 05_LogisticRegression/test/natural_logarithm.py
+++ 05_LogisticRegression/test/natural_logarithm.py
@@ -1,3 +1,13 @@
 n = 100000000
 e = (1 + (1 / n)) ** n
 print(e)
+
+
+result = 10
+for i in range(1, 10):
+    result -= 1
+    print(result)
+
+    if(result == 0):
+        print("Result is 0")
+        break
