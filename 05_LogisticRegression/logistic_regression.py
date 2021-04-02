e960abe9f309576573695622772fdd3a02584e81
Geonwoo Kim <kw.kim@xenoimpact.com>
2021-04-02 18:11:52 +0900

    [Update] - Comments unnecessary codes

--- 05_LogisticRegression/logistic_regression.py
+++ 05_LogisticRegression/logistic_regression.py
@@ -20,25 +20,6 @@ lr = 0.05
 def sigmoid(x):
     return 1 / (1 + np.e ** (-x))
 
-# for i in range(2001):
-#     for x_data, y_data in data:
-#         a_diff = x_data * (sigmoid(a * x_data + b) - y_data)
-#         b_diff = sigmoid(a * x_data + b) - y_data
-#         a = a - lr * a_diff
-#         b = b - lr * b_diff
-
-#         if i % 1000 == 0:
-#             print("Epoch = %.f, Gradient = %.04f, Intercept = %.04f" % (i, a, b))
-    
-#     plt.scatter(x_data, y_data)
-#     plt.xlim(0, 15)
-#     plt.ylim(-.1, 1.1)
-    
-#     x_range = (np.arange(0, 15, 0.1))
-#     plt.plot(np.arange(0, 15, 0.1), np.array([sigmoid(a * x + b)
-# for x in x_range]))
-# plt.show()
-
 for i in range(2001):
     for x_data, y_data in data:
         a_diff = x_data * (sigmoid(a * x_data + b) - y_data)
@@ -48,15 +29,34 @@ for i in range(2001):
 
         if i % 1000 == 0:
             print("Epoch = %.f, Gradient = %.04f, Intercept = %.04f" % (i, a, b))
-
+    
     plt.scatter(x_data, y_data)
     plt.xlim(0, 15)
     plt.ylim(-.1, 1.1)
-
+    
     x_range = (np.arange(0, 15, 0.1))
     plt.plot(np.arange(0, 15, 0.1), np.array([sigmoid(a * x + b)
 for x in x_range]))
 plt.show()
+
+# for i in range(2001):
+#     for x_data, y_data in data:
+#         a_diff = (sigmoid(a * y_data + b) + x_data)
+#         b_diff = y_data * (sigmoid(a * y_data + b) + x_data)
+#         a = a + lr * a_diff
+#         b = b + lr * b_diff
+
+#         if i % 1000 == 0:
+#             print("Epoch = %.f, Gradient = %.04f, Intercept = %.04f" % (i, a, b))
+
+#     plt.scatter(x_data, y_data)
+#     plt.xlim(0, 15)
+#     plt.ylim(-.1, 1.1)
+
+#     y_range = (np.arange(0, -15, 0.1))
+#     plt.plot(np.arange(0, -15, 0.1), np.array([sigmoid(a * x + b)
+# for x in y_range]))
+# plt.show()
 
 
 # for i in range(2001):
