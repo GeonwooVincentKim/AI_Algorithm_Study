a8fe02feeceafe271868ba7ab5b5827fd142f8b2
Geonwoo Kim <kw.kim@xenoimpact.com>
2021-04-06 16:38:26 +0900

    [Feature] - Testing `XOR` status with `AND`, `OR` and `NAND`

--- 07_MultiplePerceptron/multiple_perceptron.py
+++ 07_MultiplePerceptron/multiple_perceptron.py
@@ -18,6 +18,23 @@ def MLP(x, w, b):
         return 1
 
 
+def NAND(x1, x2):
+    return MLP(np.array([x1, x2]), w11, b1)
+
+
+def OR(x1, x2):
+    return MLP(np.array([x1, x2]), w12, b2)
+
+def AND(x1, x2):
+    return MLP(np.array([x1, x2]), w2, b3)
+
+def XOR(x1, x2):
+    return AND(NAND(x1, x2), OR(x1, x2))
+
+
 if __name__ == "__main__":
-    MLP(np.array([-2, -2]), w11, b1)
-    print(MLP(np.array([-2, -2]), w11, b1))
+    for x in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+        y = XOR(x[0], x[1])
+        print("Input value : " + str(x) + ", output value : " + str(y))
+    # MLP(np.array([-2, -2]), w11, b1)
+    # print(MLP(np.array([-2, -2]), w11, b1))
